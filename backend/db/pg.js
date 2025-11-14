bd16f02e015270aff7d1931ac5523852854cc454
alisher152 <slasher.abdullaev601@gmail.com>
2025-11-14 18:42:58 +0500

    Initial Commit

--- backend/db/pg.js
+++ backend/db/pg.js
@@ -1,48 +1,39 @@
 /* global process */
 
-const { Pool } = require('pg');
-const DatabaseError = require('./databaseError');
-const pool = new Pool({
-    user: process.env.PGUSER,
-    host: process.env.PGHOST, 
-    database: process.env.PGDATABASE,
-    password: process.env.PGPASSWORD,
-    port: process.env.PGPORT
+const { Pool } = require("pg");
+const DatabaseError = require("./databaseError");
+
+// Покажем, какие переменные реально читает Node.js
+console.log("DB CONFIG: ", {
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
-//const { Pool } = require('pg');
-//
-//const pool = new Pool({
-//    user: process.env.PGUSER,
-//    host: process.env.PGHOST,
-//    database: process.env.PGDATABASE,
-//    password: process.env.PGPASSWORD,
-//    port: process.env.PGPORT
-//});
-//
-//module.exports = {
-//    query: (text, params, callback) => {
-//        return pool.query(text, params, callback);
-//    },
-//    getClient: async () => {
-//        return await pool.connect();
-//    }
-//};
-
+// Создаем пул подключений с вашими переменными из .env
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
 
 module.exports = {
-    query: (text, params, callback) => {
-        return new Promise(function (resolve, reject) {
-            pool.query(text, params, (err, res) => {
-                if (err) {
-                    return reject(new DatabaseError(err, text));
-                }
-                if (typeof callback === "function") return resolve(callback(err, res));
-                resolve(res);
-            });
-        });
-    },
-    getClient: async () => {
-        return await pool.connect();
-    },
+  query: (text, params, callback) => {
+    return new Promise(function (resolve, reject) {
+      pool.query(text, params, (err, res) => {
+        if (err) {
+          return reject(new DatabaseError(err, text));
+        }
+        if (typeof callback === "function") return resolve(callback(err, res));
+        resolve(res);
+      });
+    });
+  },
+  getClient: async () => {
+    return await pool.connect();
+  },
 };
